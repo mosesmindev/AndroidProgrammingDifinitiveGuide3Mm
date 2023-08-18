0abc2d4808dc5e2baafa4160290d1f0ab96689cb
MosesMin <peterchenjinxu@gmail.com>
2023-08-18 22:31:25 +0800

    003 commit by MosesMin,change Part01_Base -- Chap02 & add Part01_Base -- Chap03.

--- 1_Base/03_ActivityLifecycle/GeoQuizMm/app/src/main/java/com/mosesmin/android/geoquizmm/QuizActivity.java
+++ 1_Base/03_ActivityLifecycle/GeoQuizMm/app/src/main/java/com/mosesmin/android/geoquizmm/QuizActivity.java
@@ -2,7 +2,7 @@ package com.mosesmin.android.geoquizmm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -10,6 +10,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
@@ -24,12 +27,18 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_americas,true),
             new Question(R.string.question_asia,true)
     };
+
     private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called!");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
 
         mQuestionTextView = findViewById(R.id.question_text_view);
 
@@ -54,12 +63,52 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1)%mQuestionBankArray.length;
-                //int question = mQuestionBankArray[mCurrentIndex].getTextResId();
-                //mQuestionTextView.setText(question);
                 updateQuestion();
             }
         });
         updateQuestion();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called!");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called!");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called!");
+    }
+
+    /**
+     * @param savedInstanceState
+     * 保存mCurrentIndex数据以应对设备旋转
+     */
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState!");
+        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called!");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called!");
     }
 
     private void updateQuestion(){
@@ -76,7 +125,6 @@ public class QuizActivity extends AppCompatActivity {
         }else{
             messageResId = R.string.incorrect_toast;
         }
-
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
     }
 }
