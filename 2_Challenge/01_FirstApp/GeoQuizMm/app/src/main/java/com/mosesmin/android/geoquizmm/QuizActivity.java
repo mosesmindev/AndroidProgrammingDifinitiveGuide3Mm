aa79d90ceb03fcabca3cba88d6d4241c8d6735a0
MosesMin <peterchenjinxu@gmail.com>
2023-08-18 23:05:27 +0800

    005 commit by MosesMin,add Part02_Challenge -- Chap01.

---
+++ 2_Challenge/01_FirstApp/GeoQuizMm/app/src/main/java/com/mosesmin/android/geoquizmm/QuizActivity.java
@@ -0,0 +1,40 @@
+package com.mosesmin.android.geoquizmm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+    private Button mTrueButton,mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                Toast toast= Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.BOTTOM,0,0);
+                toast.show();
+            }
+        });
+
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                Toast toast= Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,0);
+                toast.show();
+            }
+        });
+    }
+}
