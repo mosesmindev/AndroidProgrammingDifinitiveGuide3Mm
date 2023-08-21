9e7717ad659c3f4523eb2e9e8eb3e2cb407dca72
MosesMin <peterchenjinxu@gmail.com>
2023-08-22 00:22:21 +0800

    009 commit by MosesMin,add Part01_Base -- Chap05.

--- 1_Base/05_SecondActivity/GeoQuizMm/app/src/main/java/com/mosesmin/android/geoquizmm/QuizActivity.java
+++ 1_Base/05_SecondActivity/GeoQuizMm/app/src/main/java/com/mosesmin/android/geoquizmm/QuizActivity.java
@@ -1,5 +1,10 @@
 package com.mosesmin.android.geoquizmm;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +16,16 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final int RESULT_CODE_CHEAT = 0;
 
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBankArray = new Question[]{
-            new Question(R.string.question_china,true),
+            new Question(R.string.question_china,false),
             new Question(R.string.question_australia,true),
             new Question(R.string.question_oceans,true),
             new Question(R.string.question_mideast,true),
@@ -28,18 +35,18 @@ public class QuizActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called!");
+        Log.d(TAG, "onCreate(Bundle) called -- QeoQuizVersion:" + getQeoQuizVersion());
         setContentView(R.layout.activity_quiz);
 
         if (savedInstanceState != null){
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
         }
-
-        mQuestionTextView = findViewById(R.id.question_text_view);
 
         mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -57,17 +64,47 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mQuestionTextView = findViewById(R.id.question_text_view);
+
         mNextButton = findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1)%mQuestionBankArray.length;
+                mIsCheater = false;
                 updateQuestion();
             }
         });
-        updateQuestion();
 
+        mCheatButton = findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start CheatActivity
+                //Intent intent = new Intent(QuizActivity.this,CheatActivity.class);
+                boolean answerIsTrue = mQuestionBankArray[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this,answerIsTrue);
+                //startActivity(intent);
+                startActivityForResult(intent,RESULT_CODE_CHEAT);
+            }
+        });
+
+        updateQuestion();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if (requestCode == RESULT_CODE_CHEAT){
+            if (data == null){
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
+    }
+
 
     @Override
     public void onStart() {
@@ -119,11 +156,27 @@ public class QuizActivity extends AppCompatActivity {
         boolean answerTrue = mQuestionBankArray[mCurrentIndex].isAnswerTrue();
         int messageResId = 0;
 
-        if (userPressedTrue == answerTrue){
-            messageResId = R.string.correct_toast;
+        if (mIsCheater){
+            messageResId = R.string.judgment_toast;
         }else{
-            messageResId = R.string.incorrect_toast;
+            if (userPressedTrue == answerTrue){
+                messageResId = R.string.correct_toast;
+            }else{
+                messageResId = R.string.incorrect_toast;
+            }
         }
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
+    }
+
+    private String getQeoQuizVersion() {
+        try {
+            PackageManager packageManager = getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(),0);
+            return "GeoQuiz_" + packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d(TAG, "getQeoQuizVersion: " + e.toString());
+            e.printStackTrace();
+        }
+        return "GeoQuiz_V1.0.0_20230818";
     }
 }
