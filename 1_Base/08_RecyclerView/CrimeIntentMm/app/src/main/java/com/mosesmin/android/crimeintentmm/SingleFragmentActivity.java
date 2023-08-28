8a9f5941d70d5d0c88b0470da1ab7ff1b07ec7da
MosesMin <peterchenjinxu@gmail.com>
2023-08-28 22:36:21 +0800

    012 commit by MosesMin,add Part01_Base -- Chap08.

---
+++ 1_Base/08_RecyclerView/CrimeIntentMm/app/src/main/java/com/mosesmin/android/crimeintentmm/SingleFragmentActivity.java
@@ -0,0 +1,38 @@
+package com.mosesmin.android.crimeintentmm;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @Misson&Goal 代码以交朋友、传福音
+ * @ClassName SingleFragmentActivity
+ * @Description TODO
+ * @Author MosesMin
+ * @Date 2023-08-28 22:28:01
+ * @Version 1.0
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    private static final String TAG = "SingleFragmentActivity";
+
+    protected abstract Fragment createFragment();
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit();
+        }
+    }
+}
