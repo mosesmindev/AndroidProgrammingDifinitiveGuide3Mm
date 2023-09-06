d8be2b96a8465c5744b5b2ba0670ebcd5b422710
MosesMin <peterchenjinxu@gmail.com>
2023-09-07 00:29:12 +0800

    019a commit by MosesMin on 20230907,add Part01_Base -- Chap15.

---
+++ 1_Base/15_ImplicitIntents/CrimeIntentMm/app/src/main/java/com/mosesmin/android/crimeintentmm/CrimeFragment.java
@@ -0,0 +1,138 @@
+package com.mosesmin.android.crimeintentmm;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static android.widget.CompoundButton.*;
+
+/**
+ *
+ * @Misson&Goal 代码以交朋友、传福音
+ * @ClassName CrimeFragment
+ * @Description TODO
+ * @Author MosesMin
+ * @Date 2023-08-28 0:40
+ * @Version 1.0
+ *
+ */
+public class CrimeFragment extends Fragment {
+
+    private static final String ARG_CRIME_ID = "crime_id";
+    private static final String TAG = "CrimeFragment";
+    private static final String DIALOG_DATE = "DialogDate";
+
+    private static final int REQUEST_DATE = 0;
+
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckbox;
+
+    public static CrimeFragment newInstance(UUID crimeId){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID,crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //mCrime = new Crime();
+        //UUID crimeId = (UUID) getActivity().getIntent().getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+        Log.i(TAG, "onCreate: Hosting Activity is : " + getActivity().getClass().getName());
+        mCrime = CrimeLab.getInstance(getActivity()).getCrime(crimeId);
+    }
+
+    // 14.4.2 插入和更新记录 14-11  Crime 数据刷新
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        CrimeLab.getInstance(getActivity())
+                .updateCrime(mCrime);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        updateDate();
+        mDateButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePiakcerFragment dialog = DatePiakcerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this,REQUEST_DATE);
+                dialog.show(manager,DIALOG_DATE);
+            }
+        });
+
+        mSolvedCheckbox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckbox.setChecked(mCrime.isSolved());
+        mSolvedCheckbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if (requestCode == REQUEST_DATE){
+            Date date = (Date) data.getSerializableExtra(DatePiakcerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
+    }
+}
---
+++ 1_Base/15_ImplicitIntents/CrimeIntentMm/app/src/main/java/com/mosesmin/android/crimeintentmm/database/CrimeDbSchema.java
@@ -0,0 +1,30 @@
+package com.mosesmin.android.crimeintentmm.database;
+
+/**
+ *
+ * @Misson&Goal 代码以交朋友、传福音
+ * @ClassName CrimeDbSchema
+ * @Description TODO
+ * @Author MosesMin
+ * @Date 2023-09-06 00:03:17
+ * @Version 1.0
+ *
+ */
+
+// 14.1 定义schema
+public class CrimeDbSchema {
+    private static final String TAG = "CrimeDbSchema";
+
+    // 14.1 定义schema  14-1 定义CrimeTable内部类
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        // 14.1 定义schema  14-2 定义数据表字段
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
