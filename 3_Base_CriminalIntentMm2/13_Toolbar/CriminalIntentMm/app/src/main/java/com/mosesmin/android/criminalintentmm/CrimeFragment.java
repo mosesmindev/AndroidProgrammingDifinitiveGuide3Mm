437308d9a2fcb0d8fa2a4e9047517cc12881b2f2
MosesMin <peterchenjinxu@gmail.com>
2023-11-16 23:17:31 +0800

    029a commit by MosesMin on 20231116,add Part03_Base_CriminalIntentMm2 -- Chap13a.

---
+++ 3_Base_CriminalIntentMm2/13_Toolbar/CriminalIntentMm/app/src/main/java/com/mosesmin/android/criminalintentmm/CrimeFragment.java
@@ -0,0 +1,174 @@
+package com.mosesmin.android.criminalintentmm;
+
+// 代码清单7-9 导入支持库版 Fragment（CrimeFragment.java）
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static android.widget.CompoundButton.*;
+
+/**
+ * @Version: 1.0
+ * @Misson&Goal: 代码以交朋友、传福音
+ * @Project:CriminalIntentMm
+ * @Description: TODO
+ * @Author: MosesMin
+ * @Date: 2023-10-25 23:23:43
+ */
+// 代码清单7-8 继承 Fragment类（CrimeFragment.java）
+public class CrimeFragment extends Fragment {
+    private static final String TAG = "CrimeFragment";
+    // 代码清单10-6 编写 newInstance(UUID) 方法（CrimeFragment.java） -- start1
+    public static final String ARG_CRIME_ID = "crime_id";
+    // 代码清单10-6 编写 newInstance(UUID) 方法（CrimeFragment.java） -- end1
+    // 代码清单12-3 显示 DialogFragment （CrimeFragment.java） -- start1
+    private static final String DIALOG_DATE = "DialogDate";
+    // 代码清单12-3 显示 DialogFragment （CrimeFragment.java） -- end1
+
+    // 代码清单12-8 设置目标fragment（CrimeFragment.java） -- start1
+    public static final int REQUEST_CODE = 0;
+    // 代码清单12-8 设置目标fragment（CrimeFragment.java） -- end1
+
+
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    // 代码清单10-6 编写 newInstance(UUID) 方法（CrimeFragment.java） -- start2
+    public static CrimeFragment newInstance(UUID crimeId){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID,crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    // 代码清单10-6 编写 newInstance(UUID) 方法（CrimeFragment.java） -- end2
+
+    // 代码清单7-10 覆盖 Fragment.onCreate(Bundle) 方法（CrimeFragment.java）
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 代码清单10-4 获取extra数据并取得 Crime 对象（CrimeFragment.java） -- start
+        //mCrime = new Crime();
+
+        // 代码清单10-8 从argument中获取crime ID（CrimeFragment.java） -- start1
+        /*UUID crimeId = (UUID) getActivity().getIntent()
+                .getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);*/
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+        // 代码清单10-8 从argument中获取crime ID（CrimeFragment.java） -- end1
+        mCrime = CrimeLab.getInstance(getActivity()).getCrime(crimeId);
+        // 代码清单10-4 获取extra数据并取得 Crime 对象（CrimeFragment.java） -- end
+    }
+
+    // 代码清单7-11 覆盖onCreateView(...)方法（CrimeFragment.java） -- start
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime,container,false);
+
+        // 代码清单7-12 生成并使用EditText组件（CrimeFragment.java）-- start
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        // 代码清单10-5 更新视图对象（CrimeFragment.java） -- start1
+        mTitleField.setText(mCrime.getTitle());
+        // 代码清单10-5 更新视图对象（CrimeFragment.java） -- end1
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intentionally left blank 这个空格故意留空
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This space intentionally left blank 这个空格故意留空  This one too
+            }
+        });
+        // 代码清单7-12 生成并使用EditText组件（CrimeFragment.java）-- end
+
+        // 代码清单7-13 设置Button文字（CrimeFragment.java） -- start
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        // 代码清单12-13 使用 updateDate() 私有方法（CrimeFragment.java） -- start3
+        updateDate();
+        // 代码清单12-13 使用 updateDate() 私有方法（CrimeFragment.java） -- end3
+        // 代码清单12-3 显示 DialogFragment （CrimeFragment.java） -- start2
+        // mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                // 代码清单12-6 添加 newInstance() 方法（CrimeFragment.java） -- start
+                //DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment
+                        .newInstance(mCrime.getDate());
+                // 代码清单12-8 设置目标fragment（CrimeFragment.java） -- start2
+                dialog.setTargetFragment(CrimeFragment.this,REQUEST_CODE);
+                // 代码清单12-8 设置目标fragment（CrimeFragment.java） -- end2
+                // 代码清单12-6 添加 newInstance() 方法（CrimeFragment.java） -- end
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
+        // 代码清单12-3 显示 DialogFragment （CrimeFragment.java） -- end2
+
+        // 代码清单7-13 设置Button文字（CrimeFragment.java） -- end
+
+        // 代码清单7-14 监听CheckBox的变化（CrimeFragment.java） -- start
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        // 代码清单10-5 更新视图对象（CrimeFragment.java） -- start2
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
+        // 代码清单10-5 更新视图对象（CrimeFragment.java） -- end2
+        mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+        // 代码清单7-14 监听CheckBox的变化（CrimeFragment.java） -- end
+        return v;
+    }
+
+    // 代码清单12-11 响应 DatePicker 对话框（CrimeFragment.java） -- start
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if (requestCode == REQUEST_CODE){
+            Date date = (Date)data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            // 代码清单12-12 高亮选取日期按钮更新代码（CrimeFragment.java） -- start
+            // mDateButton.setText(mCrime.getDate().toString());  // Refactor → Extract Method...
+            // 代码清单12-12 高亮选取日期按钮更新代码（CrimeFragment.java） -- end
+            // 代码清单12-13 使用 updateDate() 私有方法（CrimeFragment.java） -- start2
+            updateDate();
+            // 代码清单12-13 使用 updateDate() 私有方法（CrimeFragment.java） -- end2
+        }
+    }
+
+
+    // 代码清单12-13 使用 updateDate() 私有方法（CrimeFragment.java） -- start1
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
+    }
+    // 代码清单12-13 使用 updateDate() 私有方法（CrimeFragment.java） -- end1
+    // 代码清单12-11 响应 DatePicker 对话框（CrimeFragment.java） -- end
+    // 代码清单7-11 覆盖onCreateView(...)方法（CrimeFragment.java） -- end
+}
---
+++ 3_Base_CriminalIntentMm2/13_Toolbar/CriminalIntentMm/app/src/main/java/com/mosesmin/android/criminalintentmm/CrimeListFragment.java
@@ -0,0 +1,202 @@
+package com.mosesmin.android.criminalintentmm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * @Version: 1.0
+ * @Misson&Goal: 代码以交朋友、传福音
+ * @Project:CriminalIntentMm
+ * @Description: TODO
+ * @Author: MosesMin
+ * @Date: 2023-10-30 22:03:50
+ */
+// 代码清单8-11 实现CrimeListFragment（CrimeListFragment.java） -- start
+public class CrimeListFragment extends Fragment {
+    private static final String TAG = "CrimeListFragment";
+    // 代码清单8-11 实现CrimeListFragment（CrimeListFragment.java） -- end
+
+    // 代码清单8-16 为CrimeListFragment配置视图（CrimeListFragment.java） -- start1
+    private RecyclerView mCrimeRecyclerView;
+    // 代码清单8-16 为CrimeListFragment配置视图（CrimeListFragment.java） -- end1
+
+    // 代码清单8-20 设置Adapter（CrimeListFragment.java） -- start2
+    private CrimeAdapter mAdapter;
+    // 代码清单8-20 设置Adapter（CrimeListFragment.java） -- end2
+
+    // 代码清单13-7 调用 setHasOptionsMenu 方法（CrimeListFragment.java） -- start
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    // 代码清单13-7 调用 setHasOptionsMenu 方法（CrimeListFragment.java） -- end
+
+    // 代码清单8-16 为CrimeListFragment配置视图（CrimeListFragment.java） -- start2
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        // 代码清单8-20 设置Adapter（CrimeListFragment.java） -- start3
+        updateUI();
+        // 代码清单8-20 设置Adapter（CrimeListFragment.java） -- end3
+        return view;
+    }
+    // 代码清单8-16 为CrimeListFragment配置视图（CrimeListFragment.java） -- end2
+
+
+    // 代码清单10-9 在 onResume() 方法中刷新列表项（CrimeListFragment.java） -- start1
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+    // 代码清单10-9 在 onResume() 方法中刷新列表项（CrimeListFragment.java） -- end1
+
+    // 代码清单13-6 实例化选项菜单（CrimeListFragment.java） -- start
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list,menu);
+    }
+    // 代码清单13-6 实例化选项菜单（CrimeListFragment.java） -- end
+
+    // 代码清单8-17 定义ViewHolder内部类（CrimeListFragment.java） -- start
+    private class CrimeHolder extends RecyclerView.ViewHolder
+        // 代码清单8-24 检测用户点击事件（CrimeListFragment.java） -- start1
+        implements View.OnClickListener{
+        // 代码清单8-24 检测用户点击事件（CrimeListFragment.java） -- end1
+
+        // 代码清单8-22 实现bind(Crime)方法（CrimeListFragment.java） -- start1
+        private Crime mCrime;
+        // 代码清单8-22 实现bind(Crime)方法（CrimeListFragment.java） -- end1
+
+        // 代码清单8-21 在构造方法中实例化视图组件（CrimeListFragment.java） -- start1
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        // 代码清单8-21 在构造方法中实例化视图组件（CrimeListFragment.java） -- end1
+
+        // 代码清单9-3 控制图片显示（CrimeListFragment.java） -- start1
+        private ImageView mSolvedImageView;
+        // 代码清单9-3 控制图片显示（CrimeListFragment.java） -- end1
+
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent){
+            super(inflater.inflate(R.layout.list_item_crime, parent, false));
+
+            // 代码清单8-24 检测用户点击事件（CrimeListFragment.java） -- start2
+            itemView.setOnClickListener(this);
+            // 代码清单8-24 检测用户点击事件（CrimeListFragment.java） -- end2
+
+            // 代码清单8-21 在构造方法中实例化视图组件（CrimeListFragment.java） -- start2
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            // 代码清单8-21 在构造方法中实例化视图组件（CrimeListFragment.java） -- end2
+
+            // 代码清单9-3 控制图片显示（CrimeListFragment.java） -- start2
+            mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
+            // 代码清单9-3 控制图片显示（CrimeListFragment.java） -- end2
+        }
+
+        // 代码清单8-22 实现bind(Crime)方法（CrimeListFragment.java） -- start2
+        // 每次有新的Crime要在CrimeHolder中显示时，都要调用bind方法一次。
+        public void bind(Crime crime){
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+
+            // 代码清单9-3 控制图片显示（CrimeListFragment.java） -- start3
+            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE:View.GONE);
+            // 代码清单9-3 控制图片显示（CrimeListFragment.java） -- end3
+        }
+
+        // 代码清单8-24 检测用户点击事件（CrimeListFragment.java） -- start3
+        @Override
+        public void onClick(View v) {
+            /*Toast.makeText(getActivity(),
+                    mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();*/
+            // 代码清单10-1 启动 CrimeActivity （CrimeListFragment.java） -- start
+            // 代码清单10-3 传递 Crime 实例（CrimeListFragment.java） -- start
+            //Intent intent = new Intent(getActivity(),CrimeActivity.class);
+            // 代码清单11-4 配置启动 CrimePagerActivity （CrimeListFragment.java） -- start
+            // Intent intent =CrimeActivity.newIntent(getActivity(),mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(),mCrime.getId());
+            // 代码清单11-4 配置启动 CrimePagerActivity （CrimeListFragment.java） -- end
+            // 代码清单10-3 传递 Crime 实例（CrimeListFragment.java） -- end
+            startActivity(intent);
+            // 代码清单10-1 启动 CrimeActivity （CrimeListFragment.java） -- end
+        }
+        // 代码清单8-24 检测用户点击事件（CrimeListFragment.java） -- end3
+
+        // 代码清单8-22 实现bind(Crime)方法（CrimeListFragment.java） -- end2
+    }
+    // 代码清单8-17 定义ViewHolder内部类（CrimeListFragment.java） -- end
+
+    // 代码清单8-18 创建Adapter内部类（CrimeListFragment.java） -- start
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
+        private List<Crime> mCrimes;
+        public CrimeAdapter(List<Crime> crimes){
+            mCrimes = crimes;
+        }
+        // 代码清单8-18 创建Adapter内部类（CrimeListFragment.java） -- end
+
+        // 代码清单8-19 武装CrimeAdapter（CrimeListFragment.java） -- start
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            Log.d(TAG, "onCreateViewHolder: ViewHolder+1");
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new CrimeHolder(layoutInflater,parent);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            // 代码清单8-23 调用bind(Crime)方法（CrimeListFragment.java） -- start
+            /*
+            修改 CrimeAdapter 类，使用 bind(Crime) 方法。每次 RecyclerView 要求 CrimeHolder
+            绑定对应的 Crime 时 ， 都会调用 bind(Crime) 方法
+             */
+            Crime crime =mCrimes.get(position);
+            holder.bind(crime);
+            // 代码清单8-23 调用bind(Crime)方法（CrimeListFragment.java） -- end
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+        // 代码清单8-19 武装CrimeAdapter（CrimeListFragment.java） -- end
+    }
+
+    // 代码清单8-20 设置Adapter（CrimeListFragment.java） -- start1
+    private void updateUI(){
+        CrimeLab crimeLab = CrimeLab.getInstance(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+
+        // 代码清单10-9 在 onResume() 方法中刷新列表项（CrimeListFragment.java） -- start2
+        if (mAdapter == null){
+        // 代码清单10-9 在 onResume() 方法中刷新列表项（CrimeListFragment.java） -- end2
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+            // 代码清单10-9 在 onResume() 方法中刷新列表项（CrimeListFragment.java） -- start3
+        }else{
+            mAdapter.notifyDataSetChanged();
+        }
+        // 代码清单10-9 在 onResume() 方法中刷新列表项（CrimeListFragment.java） -- end3
+
+    }
+    // 代码清单8-20 设置Adapter（CrimeListFragment.java） -- end1
+
+}
