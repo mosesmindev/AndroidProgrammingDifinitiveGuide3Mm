18f197021860692a569b4e88c125561786f87cb6
MosesMin <peterchenjinxu@gmail.com>
2023-11-24 22:17:02 +0800

    035 commit by MosesMin on 20231124,add Part03_Base_CriminalIntentMm2 -- Chap19.

---
+++ 3_Base_CriminalIntentMm2/19_Accessibility/CriminalIntentMm/app/src/main/java/com/mosesmin/android/criminalintentmm/database/CrimeDbSchema.java
@@ -0,0 +1,31 @@
+package com.mosesmin.android.criminalintentmm.database;
+
+/**
+ * @Version: 1.0
+ * @Misson&Goal: 代码以交朋友、传福音
+ * @Project:CriminalIntentMm
+ * @Description: TODO
+ * @Author: MosesMin
+ * @Date: 2023-11-20 20:30:31
+ */
+public class CrimeDbSchema {
+    private static final String TAG = "CrimeDbSchema";
+
+    // 代码清单14-1 定义 CrimeTable 内部类（CrimeDbSchema.java） -- start
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        // 代码清单14-2 定义数据表字段（CrimeDbSchema.java） -- start
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+            // 代码清单15-3 添加嫌疑人数据库字段（CrimeDbSchema.java） -- start
+            public static final String SUSPECT = "suspect";
+            // 代码清单15-3 添加嫌疑人数据库字段（CrimeDbSchema.java） -- end
+        }
+        // 代码清单14-2 定义数据表字段（CrimeDbSchema.java） -- end
+    }
+    // 代码清单14-1 定义 CrimeTable 内部类（CrimeDbSchema.java） -- end
+}
