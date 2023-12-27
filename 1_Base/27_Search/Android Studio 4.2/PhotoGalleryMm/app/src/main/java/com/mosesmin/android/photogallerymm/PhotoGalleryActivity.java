4250f09a29fb4e94661b1b5011113b6d6e2d1593
MosesMin <peterchenjinxu@gmail.com>
2023-12-27 23:02:07 +0800

    046 commit by chenjinxu on 20231227,add part01_base -- chap27.

---
+++ 1_Base/27_Search/Android Studio 4.2/PhotoGalleryMm/app/src/main/java/com/mosesmin/android/photogallerymm/PhotoGalleryActivity.java
@@ -0,0 +1,14 @@
+package com.mosesmin.android.photogallerymm;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * 代码清单25-1 activity的调整（PhotoGalleryActivity.java）
+ */
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
